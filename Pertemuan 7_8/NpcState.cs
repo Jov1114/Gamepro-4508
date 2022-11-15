c194ff68de3a21071866e132d13ba3752f60b91c
Jov1114 <108922816+Jov1114@users.noreply.github.com>
2022-11-15 22:16:03 +0700

    Add files via upload

---
+++ Pertemuan 7_8/NpcState.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NpcState : MonoBehaviour
+{
+    GameObject player;
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        anim = GetComponent<Animator> ();
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance (transform.position, player.transform.position) <4f){
+            anim.SetBool ("IsDekat", true);
+        }
+        else{
+            anim.SetBool ("IsDekat", false);
+        }
+        
+    }
+}
